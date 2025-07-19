d9c1ec2266a343b3f8d3dfa97bdccf114d69e85c
zzzolite <zzzolite@gmail.com>
2025-07-19 06:10:18 -0400

    Wipe functionality

--- app/routes/wipe.tsx
+++ app/routes/wipe.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import React, { useEffect, useState } from "react";
+import {Link, useNavigate} from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 const WipeApp = () => {
@@ -42,23 +42,33 @@ const WipeApp = () => {
     }
 
     return (
-        <div>
+        <div className="flex flex-col gap-8 p-8 w-full">
             Authenticated as: {auth.user?.username}
-            <div>Existing files:</div>
             <div className="flex flex-col gap-4">
+                <h3 className="text-lg font-bold">Existing files:</h3>
+
                 {files.map((file) => (
                     <div key={file.id} className="flex flex-row gap-4">
                         <p>{file.name}</p>
                     </div>
                 ))}
             </div>
-            <div>
+            <div className="flex gap-4">
                 <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                    className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
                     onClick={() => handleDelete()}
                 >
                     Wipe App Data
                 </button>
+
+                <Link to={"/"} className={"back-button"}>
+                    <img
+                        alt={"Logo"}
+                        src={"/icons/back.svg"}
+                        className="w-2.5 h-2.5"
+                    />
+                    <span className={"text-gray-800 text-sm font-semibold"}>Back To Home Page</span>
+                </Link>
             </div>
         </div>
     );
